3823954932603487e6fbf56a94047f4219d0bf2b
StefK <148461826+Lambersart@users.noreply.github.com>
2025-03-01 17:48:23 +0100

    soutenance2projet3

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,9 +1,3 @@
-/**
- * Ici on va avoir pour objectif de détecter le clic sur le bouton
- * -> récupérer les données qui ont été saisies par l'utilisateur
- * -> envoyer au backend/api avec un fetch avec la méthode POST
- * http://localhost:5678/api/users/login
- */
 document.addEventListener("DOMContentLoaded", () => {
   // détecter le clic sur le bouton d'envoi du formulaire et récupérer les infos
 
@@ -11,9 +5,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const boutonEnvoi = document.querySelector(".submit");
 
   const errorMessage = document.querySelector("#error-message");
-  //Vérification
-  console.log("envoi:", boutonEnvoi);
-  //console.log("Erreur", errorMessage);
 
   // 2- Ajouter le listener
   boutonEnvoi.addEventListener("click", function (event) {
@@ -21,22 +12,54 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("btn Cliqué!,connection..");
 
     // 3 - Récupérer les informations entrées par l'utilisateur
-    const emailFromForm = document.querySelector("#email").value;
-    const passwordFromForm = document.querySelector("#password").value;
+    const emailFromForm = document.querySelector("#email").value.trim();
+    const passwordFromForm = document.querySelector("#password").value.trim();
+
+    //  FUSIBLE
+    let fuse = true;
+    if (emailFromForm.length < 1) {
+      fuse = false;
+    }
+    if (passwordFromForm.length < 6) {
+      // Vérification de la longueur du mot de passe
+      fuse = false;
+    }
+    if (!validateEmail(emailFromForm)) {
+      fuse = false;
+    }
+
+    // Si une erreur est détectée, on bloque l'appel à l'API et on affiche un message d'erreur
+    if (!fuse) {
+      errorMessage.innerText = "Email ou mot de passe invalide.";
+      errorMessage.style.display = "block";
+      setTimeout(() => {
+        errorMessage.innerText = "";
+      }, 5000);
+      return; // Empêche l'appel à l'API
+    }
+
+    // Si les champs sont corrects, on cache le message d'erreur
+    errorMessage.style.display = "none";
 
     // 4 - On construit le corps de la requête
     const requestBody = {
       email: emailFromForm,
       password: passwordFromForm,
     };
-    console.log("Email saisi:", passwordFromForm);
-    console.log("mot de passe saisi!", passwordFromForm);
-    console.log("corps de la requète!!:", requestBody);
+    // console.log("Email saisi:", emailFromForm);
+    // console.log("mot de passe saisi!", passwordFromForm);
+    // console.log("corps de la requète!!:", requestBody);
 
     // 6 - On envoie notre body dans la fonction de connection
     getLogIn(requestBody);
   });
   //fonction pour gérer la connexion
+  // Fonction pour valider le format de l'email
+  function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  }
+
   async function getLogIn(requestBody) {
     try {
       console.log("envoi de la requete à l'API");
@@ -48,22 +71,20 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         body: JSON.stringify(requestBody),
       });
+
       console.log("réponse reçu de l'API:", reponse);
+      if (!reponse.ok) {
+        throw new Error("Identifiants incorrects");
+      }
       //verifier si la réponse est bien un JSON
       let data;
       try {
         data = await reponse.json();
-        console.log("données reçues de l'API", data);
+        // console.log("données reçues de l'API", data);
       } catch (jsonError) {
         throw new Error("La réponse de l'API n'est pas un JSON valide");
       }
-      if (!reponse.ok) {
-        console.error("erreur détectée:", data);
-        //data.message, sinon stringify de l'objet data
-        let errorMsg =
-          data.message || JSON.stringify(data) || "échec de la connection";
-        throw new Error(errorMsg);
-      }
+
       //Le token dans localStorage
       console.log("Stockage du token dans localStorage");
       localStorage.setItem("token", data.token);
@@ -71,7 +92,7 @@ document.addEventListener("DOMContentLoaded", () => {
       window.location.href = "index.html";
     } catch (error) {
       console.error("Erreur de connexion:", error);
-      errorMessage.textContent = error.message;
+      errorMessage.textContent = "email ou mot de passe incorrect";
       errorMessage.style.display = "block";
     }
   }
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -35,8 +35,8 @@ document.addEventListener("DOMContentLoaded", () => {
       // Parcourir les projets et les afficher
       data.forEach((project) => {
         // Filtrer par catégorie si nécessaire
-        /** Si la idCatégorie est égale à 0 alors on continue en traitant chaque projet contenu dans data 
-				OU idCategorie est différent de 0 et est égale à l'id de la catégorie du "project" intérrogé et on continue ou non */
+        /** Si la idCatégorie est égale à 0 alors on continue en traitant chaque projet contenu dans data */
+
         if (idCategorie === 0 || idCategorie === project.category.id) {
           // Créer un élément figure pour chaque projet
           const figure = document.createElement("figure");
@@ -64,11 +64,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   // Appeler la fonction au démarrage pour afficher tous les projets
   fetchProjects();
-
-  /**
-   *
-   * Générer dynamiquement les boutons de catégorie
-   */
+  // Générer dynamiquement les boutons de catégorie
 
   const categoriesArray = [];
 
@@ -124,7 +120,7 @@ document.addEventListener("DOMContentLoaded", () => {
       //affiche la barre edition
       editModeBar.style.display = "inline";
       editButtons.forEach((button) => {
-        button.style.display = "inline"; //OU block?
+        button.style.display = "inline";
       });
       // Modification du lien pour afficher "logout"
       authLink.textContent = "logout";
@@ -152,131 +148,141 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const modale = document.getElementById("modal");
   const editButton = document.getElementById("edit-button");
-  const closeModal = document.getElementById("close-modal");
-  const thumbnailGallery = document.getElementById("thumbnail-gallery");
-  const addImageButton = document.getElementById("add-image-button");
-  //Récup du h2
-  const galleryTitle = document.getElementById("gallery-title");
 
   //ouvrir la modale
   editButton.addEventListener("click", (event) => {
     // selectionner la modal
+    modale.querySelector(".modal-content").innerHTML = `
+      <h2 id="gallery-title">Galerie photo</h2>
+      <button id="close-modal" class="close-button">&times;</button>
+      <div id="thumbnail-gallery" class="thumbnail-gallery">
+        <i class="fa-solid fa-trash"></i>
+      </div>
+      <button id="add-image-button" class="add-image-button">
+        Ajouter une photo
+      </button>
+    `;
+
     //changer son style pour l'afficher
     modale.style.display = "block";
     loadThumbnails(); //charge les miniatures
-    console.log("modale ouverte! avec bouton modifier");
-  });
-  //fermer la modale avecle bouton de fermeture
-  closeModal.addEventListener("click", () => {
-    modale.style.display = "none";
-  });
-  //fermer la modale en cliquant ailleurs
-  window.addEventListener("click", (event) => {
-    if (event.target === modale) {
+
+    const thumbnailGallery = document.getElementById("thumbnail-gallery");
+    const addImageButton = document.getElementById("add-image-button");
+    const galleryTitle = document.getElementById("gallery-title");
+    const closeModal = document.getElementById("close-modal");
+
+    //fermer la modale avecle bouton de fermeture
+    closeModal.addEventListener("click", () => {
       modale.style.display = "none";
-    }
-  });
-
-  //Pour charger les miniatures----fonction dédiée----------
-  async function fetchProjectsThumbnails() {
-    try {
-      const response = await fetch(apiUrl);
-      if (!response.ok) {
-        throw new Error(`Erreur HTTP : ${response.status}`);
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error(
-        "Erreur lors de la récupération des projets pour les miniatures :",
-        error
-      );
-      return [];
-    }
-  }
-  // --- Fonction pour supprimer un projet ---
-  async function deleteProject(imageId) {
-    try {
-      // const token = localStorage.getItem("authToken");
-      console.log("token récupéré:", token);
-
-      const response = await fetch(
-        `http://localhost:5678/api/works/${imageId}`,
-        {
-          method: "DELETE",
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      if (!response.ok) {
-        throw new Error(
-          `Erreur lors de la suppression du projet (statut ${response.status})`
-        );
-      }
-      console.log(`Projet ${imageId} supprimé avec succès`);
-      //supprimer du DOM
-      const imageElement = document.querySelector(
-        `[data-project-id="${imageId}"]`
-      );
-
-      if (imageElement) {
-        imageElement.parentElement.remove(); // Supprime le conteneur de l'image
-      }
-    } catch (error) {
-      console.error("Erreur lors de la suppression du projet :", error);
-    }
-  }
-
-  //--Ajout Charger les miniatures
-  function loadThumbnails() {
-    fetchProjectsThumbnails().then((projects) => {
-      thumbnailGallery.innerHTML = ""; // Vider le conteneur des miniatures
-      projects.forEach((project) => {
-        const container = document.createElement("div");
-        container.classList.add("thumbnail-container");
-
-        const img = document.createElement("img");
-        img.src = project.imageUrl;
-        img.alt = project.title;
-        img.dataset.projectId = project.id;
-
-        // Création de l'icône de suppression
-        const trashIcon = document.createElement("i");
-        trashIcon.classList.add("fa-solid", "fa-trash", "trash-icon");
-
-        // Ajout de l'événement de suppression
-        trashIcon.addEventListener("click", (event) => {
-          event.stopPropagation(); // Empêche l'event de se propager à l'image
-          if (confirm(`Voulez-vous supprimer l'image "${project.title}" ?`)) {
-            // Supprime immédiatement l'image de la modale
-            const container = event.target.closest(".thumbnail-container");
-            if (container) {
-              container.remove();
-            }
-
-            deleteProject(project.id).then(() => {
-              loadThumbnails(); // Recharger les miniatures après suppression
-              // Recharge la galerie principale pour mettre à jour l'affichage global
-              fetchProjects();
-            });
-          }
-        });
-
-        container.appendChild(img);
-        container.appendChild(trashIcon);
-        thumbnailGallery.appendChild(container);
-      });
+      modale.querySelector(".modal-content").innerHTML = "";
     });
-  }
+    //fermer la modale en cliquant ailleurs
+    window.addEventListener("click", (event) => {
+      if (event.target === modale) {
+        modale.style.display = "none";
+      }
+    });
 
-  // Fonction pour afficher le formulaire d'ajout de photo avec bouton HTML
-  function showAddPhotoForm() {
-    //masque le titre et le formulaire
-    galleryTitle.style.display = "none";
-    addImageButton.style.display = "none";
+    //Pour charger les miniatures ----fonction dédiée----------
+    async function fetchProjectsThumbnails() {
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error(`Erreur HTTP : ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error(
+          "Erreur lors de la récupération des projets pour les miniatures :",
+          error
+        );
+        return [];
+      }
+    }
+    // --- Fonction pour supprimer un projet ---
+    async function deleteProject(imageId) {
+      try {
+        // token
+        console.log("token récupéré:", token);
 
-    thumbnailGallery.innerHTML = `
+        const response = await fetch(
+          `http://localhost:5678/api/works/${imageId}`,
+          {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        if (!response.ok) {
+          throw new Error(
+            `Erreur lors de la suppression du projet (statut ${response.status})`
+          );
+        }
+        //supprimer du DOM
+        const imageElement = document.querySelector(
+          `[data-project-id="${imageId}"]`
+        );
+
+        if (imageElement) {
+          imageElement.parentElement.remove(); // Supprime le conteneur de l'image
+        }
+      } catch (error) {
+        console.error("Erreur lors de la suppression du projet :", error);
+      }
+    }
+
+    //--Ajout Charger les miniatures
+    function loadThumbnails() {
+      fetchProjectsThumbnails().then((projects) => {
+        thumbnailGallery.innerHTML = ""; // Vider le conteneur des miniatures
+        projects.forEach((project) => {
+          const container = document.createElement("div");
+          container.classList.add("thumbnail-container");
+
+          const img = document.createElement("img");
+          img.src = project.imageUrl;
+          img.alt = project.title;
+          img.dataset.projectId = project.id;
+
+          // Création de l'icône de suppression
+          const trashIcon = document.createElement("i");
+          trashIcon.classList.add("fa-solid", "fa-trash", "trash-icon");
+
+          // Ajout de l'événement de suppression
+          trashIcon.addEventListener("click", (event) => {
+            event.stopPropagation(); // Empêche l'event de se propager à l'image
+            if (confirm(`Voulez-vous supprimer l'image "${project.title}" ?`)) {
+              // Supprime immédiatement l'image de la modale
+              const container = event.target.closest(".thumbnail-container");
+              if (container) {
+                container.remove();
+              }
+
+              deleteProject(project.id).then(() => {
+                loadThumbnails(); // Recharger les miniatures après suppression
+                // Recharge la galerie principale pour mettre à jour l'affichage global
+                fetchProjects();
+              });
+            }
+          });
+
+          container.appendChild(img);
+          container.appendChild(trashIcon);
+          thumbnailGallery.appendChild(container);
+        });
+      });
+    }
+
+    // Fonction pour afficher le formulaire d'ajout de photo avec bouton HTML
+    function showAddPhotoForm() {
+      //masque le titre et le formulaire
+      galleryTitle.style.display = "none";
+      addImageButton.style.display = "none";
+
+      thumbnailGallery.innerHTML = `
       <div id="add-photo-form">
         <button id="back-button" class="back-button">
           <i class="fa-solid fa-arrow-left"></i>
@@ -284,19 +290,19 @@ document.addEventListener("DOMContentLoaded", () => {
         <h2>Ajout photo</h2>
         <form id="photo-form">
           <div class="form-group upload-box">
-            <label for="photo-upload" class="upload-label">
+            <label for="photo-upload" class="upload-label" id="previewImageContainer">
               <i class="fa-solid fa-image"></i>
               <p>jpg, png : 4mo max</p>
             </label>
-            <input type="file" id="photo-upload" class="photo-input" accept="image/png, image/jpeg" required />
+            <input type="file" id="photo-upload" class="photo-input" accept="image/png, image/jpeg"/>
           </div>
           <div class="form-group">
             <label for="photo-title">Titre</label>
-            <input type="text" id="photo-title" class="form-input" placeholder="Titre de la photo" required />
+            <input type="text" id="photo-title" class="form-input" placeholder="Titre de la photo"/>
           </div>
           <div class="form-group">
             <label for="photo-category">Catégorie</label>
-            <select id="photo-category" class="form-input" required>
+            <select id="photo-category" class="form-input">
               <option value="">Sélectionnez une catégorie</option>
               ${categoriesArray
                 .map(
@@ -306,72 +312,119 @@ document.addEventListener("DOMContentLoaded", () => {
                 .join("")}
             </select>
           </div>
+          <div id="form-message"></div>
           <button type="submit" class="submit-button">Valider</button>
         </form>
       </div>
     `;
 
-    // Gestion du bouton retour
-    document.getElementById("back-button").addEventListener("click", () => {
-      // titre et h2
-      galleryTitle.style.display = "block";
-      addImageButton.style.display = "block";
-      loadThumbnails(); // Recharger les miniatures
-    });
+      previewImage();
 
-    // Gestion du formulaire
-    document
-      .getElementById("photo-form")
-      .addEventListener("submit", (event) => {
-        event.preventDefault(); // Empêche le rechargement de la page
-
-        // Récupère les données du formulaire
-        const formData = new FormData();
-        const photoInput = document.getElementById("photo-upload");
-        const titleInput = document.getElementById("photo-title");
-        const categoryInput = document.getElementById("photo-category");
-
-        formData.append("image", photoInput.files[0]);
-        formData.append("title", titleInput.value);
-        formData.append("category", categoryInput.value);
-
-        // Envoie les données à l'API
-        //  const token =
-        //    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTczOTM3MTIyNywiZXhwIjoxNzM5NDU3NjI3fQ.-53NSROjP_yrG58sHI480G-BeD0T_wLj2Tp3UbYgIUo"; // Remplace par ton vrai token
-        console.log("token récupéreé!");
-
-        fetch("http://localhost:5678/api/works", {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${token}`, // Ajout du token
-          },
-          body: formData,
-        })
-          .then((response) => {
-            // réponse brute
-            if (response.ok) {
-              return response.json();
-            } else {
-              console.log(response);
-              throw new Error("Erreur lors de l'ajout de la photo.");
-            }
-          })
-          .then((data) => {
-            alert("Photo ajoutée avec succès !");
-
-            galleryTitle.style.display = "block";
-            addImageButton.style.display = "block";
-            loadThumbnails(); // Recharge la galerie
-            fetchProjects();
-          })
-          .catch((error) => console.error(error));
+      // Gestion du bouton retour
+      document.getElementById("back-button").addEventListener("click", () => {
+        // titre et h2
+        galleryTitle.style.display = "block";
+        addImageButton.style.display = "block";
+        loadThumbnails(); // Recharger les miniatures
       });
-  }
-  // Attacher l'événement au bouton "Ajouter une photo" du HTML
-  if (addImageButton) {
-    addImageButton.addEventListener("click", () => {
-      console.log("Bouton 'Ajouter une photo' cliqué !");
-      showAddPhotoForm();
-    });
-  }
+
+      // Gestion du formulaire
+      document
+        .getElementById("photo-form")
+        .addEventListener("submit", (event) => {
+          event.preventDefault(); // Empêche le rechargement de la page
+          const formMessage = document.getElementById("form-message");
+
+          // Récupère les données du formulaire
+          const formData = new FormData();
+          const photoInput = document.getElementById("photo-upload");
+          const titleInput = document.getElementById("photo-title");
+          const categoryInput = document.getElementById("photo-category");
+
+          let fuse = true;
+          if (photoInput.files.length < 1) {
+            fuse = false;
+          }
+          if (titleInput.value.length < 1) {
+            fuse = false;
+          }
+          if (categoryInput.value.length < 1) {
+            fuse = false;
+          }
+
+          if (!fuse) {
+            formMessage.innerText =
+              "Il y a des erreurs, contrôlez votre saisie.";
+            setTimeout(() => {
+              formMessage.innerText = "";
+            }, 5000);
+            return false;
+          }
+
+          formData.append("image", photoInput.files[0]);
+          formData.append("title", titleInput.value);
+          formData.append("category", categoryInput.value);
+
+          // Envoie les données à l'API
+
+          fetch("http://localhost:5678/api/works", {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${token}`, // Ajout du token
+            },
+            body: formData,
+          })
+            .then((response) => {
+              // réponse brute
+              if (response.ok) {
+                return response.json();
+              } else {
+                console.log(response);
+                throw new Error("Erreur lors de l'ajout de la photo.");
+              }
+            })
+            .then((data) => {
+              galleryTitle.style.display = "block";
+              addImageButton.style.display = "block";
+              loadThumbnails(); // Recharge la galerie
+              fetchProjects();
+            })
+            .catch((error) => console.error(error));
+        });
+    }
+    // Attacher l'événement au bouton "Ajouter une photo" du HTML
+    if (addImageButton) {
+      addImageButton.addEventListener("click", () => {
+        console.log("Bouton 'Ajouter une photo' cliqué !");
+        showAddPhotoForm();
+      });
+    }
+
+    function previewImage() {
+      const fileInput = document.getElementById("photo-upload");
+      let previewImageContainer = document.getElementById(
+        "previewImageContainer"
+      );
+
+      fileInput.addEventListener("change", function (event) {
+        const file = event.target.files[0];
+
+        if (file.type == "image/png" || file.type == "image/jpeg") {
+          const reader = new FileReader();
+          reader.onload = function (e) {
+            previewImageContainer.innerHTML = "";
+            const img = document.createElement("img");
+            img.src = e.target.result;
+            img.alt = "Aperçu de l'image";
+            previewImageContainer.appendChild(img);
+          };
+          reader.readAsDataURL(file);
+        } else {
+          fileInput.value = "";
+          previewImageContainer.innerHTML =
+            "<p> ! Seuls les fichiers PNG ou JPG sont autorisés.</p>";
+        }
+      });
+    }
+  });
 });
